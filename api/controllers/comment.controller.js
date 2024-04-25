bf9fe06418460306ed65cd3f653887f7eb0268fe
fiona-spencer <fionagspencer@gmail.com>
2024-04-25 13:58:11 -0400

    show comments of a post

--- api/controllers/comment.controller.js
+++ api/controllers/comment.controller.js
@@ -22,3 +22,27 @@ export const createComment = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getPostComments = async (req, res, next) => {
+  try {
+    const comments = await Comment.find({ postId: req.params.postId }).sort({
+      createAt: -1,
+    });
+    res.status(200).json(comments);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if(!user){
+      return next(errorHandler(404, 'User not found'));
+    }
+    const { password, ...rest} = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
